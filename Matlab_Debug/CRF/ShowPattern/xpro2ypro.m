5c72e15663e4ee9f021e1652bc8c05b83d64752a
Rukun Qiao <1601111330@pku.edu.cn>
2017-06-23 21:56:43 +0800

    Finish pattern color draw part

--- Matlab_Debug/CRF/ShowPattern/xpro2ypro.m
+++ Matlab_Debug/CRF/ShowPattern/xpro2ypro.m
@@ -1,7 +1,7 @@
-function y_p = xpro2ypro(x_c, y_c, x_p, lineA, lineB, lineC)
+function y_p = xpro2ypro(x_c, y_c, x_p, lineA, lineB)
     paraA = lineA(y_c, x_c);
     paraB = lineB(y_c, x_c);
-    paraC = - lineC(y_c, x_c);
+    paraC = - 1;
 
     y_p = - (paraA / paraB) * x_p - (paraC / paraB);
 end
