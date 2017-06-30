6cd5aa7cbab1046ce782c6e0691d627826594491
Rukun Qiao <1601111330@pku.edu.cn>
2017-06-30 10:18:46 +0800

    Set Color Recognization

--- Matlab_Debug/CRF/20170619/MainLoop.m
+++ Matlab_Debug/CRF/20170619/MainLoop.m
@@ -12,30 +12,17 @@ last_x_gt = load([main_file_path, pro_x_file_path, ...
 last_y_gt = load([main_file_path, pro_y_file_path, ...
     pro_y_file_name, '1', file_suffix]);
 
-last_depth_mat = zeros(size(last_camera_image));
-valid_mask = ones(size(last_camera_image));
-for h = 1:CAMERA_HEIGHT
-    for w = 1:CAMERA_WIDTH
-        x_p = last_x_gt(h, w);
-        y_p = last_y_gt(h, w);
-        if (x_p > 0) && (y_p > 0)
-            last_depth_mat(h, w) = xpro2depth(w, h, x_p);
-        else
-            valid_mask(h, w) = 0;
-        end
-    end
-end
+
 for h = viewportMatrix(2, 1):viewportMatrix(2, 2)
     for w = viewportMatrix(1, 1):viewportMatrix(1, 2)
-        if valid_mask(h, w) == 0
+        if (last_x_gt(h, w) <= 0)
             last_x_gt(h, w) = (last_x_gt(h-1, w) + last_x_gt(h+1, w)) / 2;
-            last_depth_mat(h, w) = last_depth_mat(h, w - 1);
         end
     end
 end
-
-% last_depth_mat = load([main_file_path, depth_file_path, ...
-%     depth_file_name, '1', file_suffix]);
+[last_depth_mat, last_valid_mask] = FillDepthMat(last_x_gt, ...
+    last_y_gt, ...
+    viewportMatrix);
 
 %% Iteration part
 for frame_idx = 2:2
@@ -43,31 +30,23 @@ for frame_idx = 2:2
     now_camera_image = imread([main_file_path, cam_img_file_path, ...
         cam_img_file_name, num2str(frame_idx), cam_file_suffix]);
     now_camera_image = double(now_camera_image) / 255.0;
+    % now_camera_image = NormalizeCameraImage(now_camera_image, viewportMatrix);
     now_x_gt = load([main_file_path, pro_x_file_path, ...
         pro_x_file_name, num2str(frame_idx), file_suffix]);
     now_y_gt = load([main_file_path, pro_y_file_path, ...
         pro_y_file_name, num2str(frame_idx), file_suffix]);
 
-    now_depth_mat = zeros(size(now_camera_image));
-    now_valid_mask = ones(size(now_camera_image));
-    for h = 1:CAMERA_HEIGHT
-        for w = 1:CAMERA_WIDTH
-            x_p = now_x_gt(h, w);
-            y_p = now_y_gt(h, w);
-            if (x_p > 0) && (y_p > 0)
-                now_depth_mat(h, w) = xpro2depth(w, h, x_p);
-            else
-                now_valid_mask(h, w) = 0;
-            end
-        end
-    end
     for h = viewportMatrix(2, 1):viewportMatrix(2, 2)
         for w = viewportMatrix(1, 1):viewportMatrix(1, 2)
-            if now_valid_mask(h, w) == 0
-                now_depth_mat(h, w) = now_depth_mat(h, w - 1);
+            if (now_x_gt(h, w) <= 0)
+                now_x_gt(h, w) = (now_x_gt(h-1, w) + now_x_gt(h+1, w)) / 2;
             end
         end
     end
+    [now_depth_mat, now_valid_mask] = FillDepthMat(now_x_gt, ...
+        now_y_gt, ...
+        viewportMatrix);
+    
 
     %% BeliefField
     % Generate Q field and initialize it
@@ -95,7 +74,7 @@ for frame_idx = 2:2
     fprintf('%d: initialized.\n', frame_idx);
     
     % Iteration
-    for i_idx = 1:6
+    for i_idx = 1:10
         beliefField = BeliefFieldIterationFast(beliefField, ...
             pattern, ...
             last_depth_mat, ...
