c14b81fd8d50b860046cd11e423a3f4acc4cb2ad
Rukun Qiao <1601111330@pku.edu.cn>
2017-07-26 00:01:38 +0800

    Add myDebug part2

--- Matlab_Debug/CRF/20160720/Untitled2.m
+++ Matlab_Debug/CRF/20160720/Untitled2.m
@@ -5,6 +5,7 @@ now_camera_mat = camera_image{frame_idx, 1};
 color_mat = zeros(size(last_camera_mat));
 for h = viewportMatrix(2,1):viewportMatrix(2,2)
     for w = viewportMatrix(1,1):viewportMatrix(1,2)
+        if abs(now_camera_mat(h, w) - 0.5) < 0.2
             color_mat(h, w) = 1;
         end
     end
