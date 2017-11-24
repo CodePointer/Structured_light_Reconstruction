2b9f939238877305e9d38371154f3de2d0de1a1a
Rukun Qiao <1601111330@pku.edu.cn>
2017-11-24 21:08:36 +0800

    Convert StereoMatching to OpenCV version

--- MatlabVersion/StereoMatching/Gene_GeneralParameters.m
+++ MatlabVersion/StereoMatching/Gene_GeneralParameters.m
@@ -19,20 +19,10 @@ CalibMat.cam_1_mat = CalibMat.cam_1 * [CalibMat.rot, CalibMat.trans];
 % CamInfo set
 CamInfo.HEIGHT = 1024;
 CamInfo.WIDTH = 1280;
-% CamInfo.range_mat = [434, 825+40; 463, 809];
-CamInfo.range_mat = [0, CamInfo.WIDTH; 0, CamInfo.HEIGHT];
-CamInfo.R_HEIGHT = CamInfo.range_mat(2,2) - CamInfo.range_mat(2,1) + 1;
-CamInfo.R_WIDTH = CamInfo.range_mat(1,2) - CamInfo.range_mat(1,1) + 1;
-CamInfo.coord_trans = zeros(CamInfo.R_HEIGHT*CamInfo.R_WIDTH, 2);
-for h = 1:CamInfo.R_HEIGHT
-    for w = 1:CamInfo.R_WIDTH
-        CamInfo.coord_trans((h-1)*CamInfo.R_WIDTH + w, :) = [h,w];
-    end
-end
-CamInfo.win_rad = 9;
+CamInfo.win_rad = 7;
 
 % FilePath
-FilePath.main_file_path = 'E:/Structured_Light_Data/20171121/Plane_MR/';
+FilePath.main_file_path = 'E:/Structured_Light_Data/20171123/StatueMRC_2/';
 FilePath.xpro_file_path = 'pro/';
 FilePath.xpro_file_name = 'xpro_mat';
 FilePath.ypro_file_path = 'pro/';
@@ -44,16 +34,16 @@ FilePath.img_file_suffix = '.png';
 FilePath.output_file_path = 'result/';
 FilePath.output_file_name = 'pc';
 FilePath.output_file_suffix = '.txt';
-pattern = imread([FilePath.main_file_path, 'pattern_2size4color0.png']);
+pattern = imread([FilePath.main_file_path, 'pattern_2size2color0.png']);
 
 % Other ParaSet
-ParaSet.M = zeros(CamInfo.R_HEIGHT*CamInfo.R_WIDTH, 3);
-ParaSet.D = zeros(CamInfo.R_HEIGHT*CamInfo.R_WIDTH, 3);
-for h_cam = 1:CamInfo.R_HEIGHT
-    for w_cam = 1:CamInfo.R_WIDTH
-        cvec_idx = (h_cam-1)*CamInfo.R_WIDTH + w_cam;
-        x_cam = w_cam + CamInfo.range_mat(1,1) - 1 - 1;
-        y_cam = h_cam + CamInfo.range_mat(2,1) - 1 - 1;
+ParaSet.M = zeros(CamInfo.HEIGHT*CamInfo.WIDTH, 3);
+ParaSet.D = zeros(CamInfo.HEIGHT*CamInfo.WIDTH, 3);
+for h_cam = 1:CamInfo.HEIGHT
+    for w_cam = 1:CamInfo.WIDTH
+        cvec_idx = (h_cam-1)*CamInfo.WIDTH + w_cam;
+        x_cam = w_cam - 1;
+        y_cam = h_cam - 1;
         tmp_vec = [(x_cam - CalibMat.cam_0(1,3)) / CalibMat.cam_0(1,1); ...
             (y_cam - CalibMat.cam_0(2,3)) / CalibMat.cam_0(2,2); ...
             1];
@@ -61,7 +51,7 @@ for h_cam = 1:CamInfo.R_HEIGHT
         ParaSet.D(cvec_idx,:) = CalibMat.cam_1_mat(:,4)';
     end
 end
-ParaSet.lumi_thred = 30;
+ParaSet.lumi_thred = 40;
 
 total_frame_num = 40;
 
--- MatlabVersion/StereoMatching/MainLoop.m
+++ MatlabVersion/StereoMatching/MainLoop.m
@@ -1,9 +1,15 @@
 clear;
+warning('off');
 
 part_PreProcess;
 
-for h_0 = CamInfo.win_rad+1:CamInfo.HEIGHT-CamInfo.win_rad
-  for w_0 = CamInfo.win_rad+1:CamInfo.WIDTH-CamInfo.win_rad
+% for h_0 = CamInfo.win_rad+1:CamInfo.HEIGHT-CamInfo.win_rad
+%   for w_0 = CamInfo.win_rad+1:CamInfo.WIDTH-CamInfo.win_rad
+for h_0 = 182:CamInfo.HEIGHT-CamInfo.win_rad
+  for w_0 = 737:CamInfo.WIDTH-CamInfo.win_rad
+    if (h_0 == 144) && (w_0 == 591)
+      fprintf('');
+    end
     if (EpiLine.lineA(h_0,w_0) == 0) && (EpiLine.lineB(h_0,w_0) == 0)
       continue;
     end
@@ -16,8 +22,9 @@ for h_0 = CamInfo.win_rad+1:CamInfo.HEIGHT-CamInfo.win_rad
     if (max_0 - min_0 < ParaSet.lumi_thred)
       continue;
     end
+    patch_0 = (patch_0 - min_0) / (max_0 - min_0);
 
-    mask_mat(h_0,w_0) = 1;
+    % mask_mat(h_0,w_0) = 1;
     cvec_idx = (h_0-1)*CamInfo.WIDTH + w_0;
     M = ParaSet.M(cvec_idx,:);
     D = ParaSet.D(cvec_idx,:);
@@ -29,14 +36,21 @@ for h_0 = CamInfo.win_rad+1:CamInfo.HEIGHT-CamInfo.win_rad
     min_depth = 0;
     % epi_show = cam_mat_1;
     for w_1 = CamInfo.win_rad+1:CamInfo.WIDTH-CamInfo.win_rad
-      depth = - (D(1)-D(3)*w_1) / (M(1)-M(3)*w_1);
-      h_center = round(-A/B * w_1 + 1/B);
+      x_1 = w_1 - 1;
+      depth = - (D(1)-D(3)*x_1) / (M(1)-M(3)*x_1);
+      h_center = round(-A/B * x_1 + 1/B + 1);
       if (h_center <= 0) || (h_center > CamInfo.HEIGHT)
         continue;
+      end
+      if w_1 == 547
+        a = 1;
       end
       % epi_show(h_center,w_1) = 255;
       for h_1 = h_center-2:h_center+2
         if (h_1 >= CamInfo.win_rad+1) && (h_1 <= CamInfo.HEIGHT-CamInfo.win_rad)
+          if (h_1 == 226) && (w_1 == 401)
+            fprintf('');  
+          end
           patch_1 = cam_mat_1(...
               h_1 - CamInfo.win_rad:h_1 + CamInfo.win_rad, ...
               w_1 - CamInfo.win_rad:w_1 + CamInfo.win_rad);
@@ -45,6 +59,7 @@ for h_0 = CamInfo.win_rad+1:CamInfo.HEIGHT-CamInfo.win_rad
           if (max_1 - min_1 < ParaSet.lumi_thred)
               continue;
           end
+          patch_1 = (patch_1 - min_1) / (max_1 - min_1);
           error_value = sum(sum((patch_0 - patch_1).^2));
           if error_value < min_val
             min_val = error_value;
@@ -62,7 +77,7 @@ for h_0 = CamInfo.win_rad+1:CamInfo.HEIGHT-CamInfo.win_rad
   end
   fprintf('.');
   if mod(h_0, 20) == 0
-    imshow(depth_mat,[30,50]);
+    imshow(depth_mat,[30,70]);
     pause(0.1);
   end
   if mod(h_0, 80) == 0
@@ -71,4 +86,4 @@ for h_0 = CamInfo.win_rad+1:CamInfo.HEIGHT-CamInfo.win_rad
 end
 fprintf('\n');
 % output
-% save status.mat
+save status.mat
